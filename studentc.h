119d14d67376afb82a3c0e75975c2dfcfdaccd4b
Oleg <olnayd@gmail.com>
2016-09-02 00:17:39 +0300

    Первый шаг

---
+++ studentc.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <valarray>
+
+class Student
+{
+private:
+	typedef std::valarray<double> ArrayDb;
+	std::string name;
+	ArrayDb scores;
+	//Метод для вывода оценок
+	std::ostream & arr_out(std::ostream &os) const;
+public:
+	Student():name("Null Student"), scores() {};
+	explicit Student(const std::string& s):name(s),scores() {};
+	explicit Student(int n):name("Nully"), scores(n) {};
+	Student(const std::string& s, int n):name(s),scores(n) {};	
+	Student(const std::string& s, const ArrayDb& db):name(s),scores(db) {};
+	Student(const char *str,const double *pd, int n):name(str),scores(pd,n) {};
+	~Student(){};
+	double Average() const;
+	const std::string &Name() const;
+	double &operator[](int i);
+	double operator[](int i) const;
+	//Друзья 
+	//Ввод
+	friend std::istream &operator>>(std::istream &is, Student &stu); //1 слово
+	friend std::istream &getline(std::istream &is,Student &stu);
+	//1 строка
+	//Вывод
+	friend std::ostream &operator<<(std::ostream &os,const Student &stu);
+};
